acb2acd008f6019aef66cd4bb4c77646c5c7cae3
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-05-13 08:11:07 +0300

    adds attributes and properties

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,42 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
+    def __init__(self, name="Unknown", breed="Pug"):  #
+        self.name = name        
+        self.breed = breed
+
+    #@property
+    def get_name(self):
+        return self._name
+
+    #@name.setter
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    #@property
+    def get_breed(self):
+        return self._breed
+
+    #@breed.setter
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
     pass
+
+
+snopy =Dog("andrew","Pug")
+print(snopy.name)
+snopy.name = "gggjfjggjfsgjjgsgjgsggjkhsjyugfsurygksygsgylseygriurliuhgeilsuglsgslieugrluiselrgls"
+print(snopy.name)
+snopy.name = "1"
+print(snopy.name)
+print(snopy.breed)
+snopy.breed = "Damascus"
+print(snopy.breed)
